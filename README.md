470da0badf7f284127bbead071ed891d38d64a90
marioquiroga32 <marioqui135@gmail.com>
2023-04-25 17:59:39 +0200

    Initialize project

---
+++ README.md
@@ -0,0 +1,15 @@
+## Description
+This project is a simple example of how to use the [Docker](https://www.docker.com/) containerization platform to build a Subquery project that indexes data from Shiden and Astar blockchains, and exposes the data through a GraphQl API.
+
+### How to run
+First, install dependencies: 
+```
+yarn
+```
+Then, run the project:
+``` 
+yarn dev 
+```
+
+### Open the GraphQl Playground
+Open the GraphQL Playground at http://localhost:3000
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,47 @@
+import { Approval, Transaction } from "../types";
+import {
+  FrontierEvmEvent,
+  FrontierEvmCall,
+} from "@subql/frontier-evm-processor";
+import { BigNumber } from "ethers";
+
+// Setup types from ABI
+type TransferEventArgs = [string, string, BigNumber] & {
+  from: string;
+  to: string;
+  value: BigNumber;
+};
+
+type ApproveCallArgs = [string, BigNumber] & {
+  _spender: string;
+  _value: BigNumber;
+};
+
+export async function handleEvmEvent(
+  event: FrontierEvmEvent<TransferEventArgs>
+): Promise<void> {
+  const transaction = Transaction.create({
+    id: `${event.blockNumber}-${event.transactionHash}-${event.logIndex}`,
+    value: event.args?.value.toBigInt(),
+    from: event.args?.from,
+    to: event.args?.to,
+    blockHeight: event.blockNumber.toString(),
+    contractAddress: event.address,
+    transactionHash: event.transactionHash
+  });
+  await transaction.save();
+}
+
+export async function handleEvmCall(
+  event: FrontierEvmCall<ApproveCallArgs>
+): Promise<void> {
+  const approval = Approval.create({
+    id: event.hash,
+    owner: event.from,
+    value: event.args._value.toBigInt(),
+    spender: event.args._spender,
+    contractAddress: event.to,
+  });
+
+  await approval.save();
+}
